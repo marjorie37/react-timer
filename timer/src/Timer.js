40f3f72de48ad94a0cb53a769c695711161b0d69
Marjorie <marjorieme@hotmail.com>
2018-05-28 16:30:48 +0200

    app timer

---
+++ timer/src/Timer.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { timer: 0 };
+  }
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
+  componentDidUpdate() {
+    console.log("un update a eu lieu");
+  }
+
+  tick() {
+    this.setState({
+      timer: this.state.timer + 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>My timer :</h2>
+        <h1 style={{ color: "red" }}>{this.state.timer}</h1>
+      </div>
+    );
+  }
+}
+
+export default Timer;
